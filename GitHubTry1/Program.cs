90411457fb110912677aa974470b3ae71288cdf6
Тимофей Дорогой <timaopus@gmail.com>
2020-06-24 19:00:21 +0700

    Initializing

---
+++ GitHubTry1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubTry1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
